3543c360168dc69c6d1e78d6f53d7519c6ba4c2e
kapustazh <matvey.nesterenko@gmail.com>
2025-09-25 22:53:44 +0200

    upd

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,10 +6,36 @@
 /*   By: atvii <atvii@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 23:29:02 by atvii             #+#    #+#             */
-/*   Updated: 2025/09/06 23:56:07 by atvii            ###   ########.fr       */
+/*   Updated: 2025/09/25 22:43:38 by atvii            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*new_lst;
+// 	t_list	*new_elem;
+// 	void	*new_content;
+
+// 	if (!lst || !f || !del)
+// 		return (NULL);
+// 	new_lst = NULL;
+// 	while (lst)
+// 	{
+// 		new_content = f(lst->content);
+// 		new_elem = ft_lstnew(new_content);
+// 		if (!new_elem)
+// 		{
+// 			del(new_content);
+// 			ft_lstclear(&new_lst, del);
+// 			return (NULL);
+// 		}
+// 		ft_lstadd_back(&new_lst, new_elem);
+// 		lst = lst->next;
+// 	}
+// 	return (new_lst);
+// }
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
@@ -18,7 +44,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_elem;
 	void	*new_content;
 
-	if (!lst || !f || !del)
+	if (!f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
@@ -27,7 +53,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new_elem = ft_lstnew(new_content);
 		if (!new_elem)
 		{
-			del(new_content);
+			if (new_content)
+				del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
